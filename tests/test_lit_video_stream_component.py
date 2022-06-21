63993ceefb0aa37b31d66c902c33360bf484699c
Kaushik B <kaushikbokka@gmail.com>
2022-06-21 15:03:38 +0100

    Fix tests

--- tests/test_lit_video_stream_component.py
+++ tests/test_lit_video_stream_component.py
@@ -5,4 +5,4 @@ from lit_video_stream.component import LitVideoStream
 def test_placeholder_component():
     # TODO: improve testing
     video_stream_component = LitVideoStream()
-    assert str(video_stream_component.features_path) == os.path.join(os.getcwd(), ".storage/features.pt")
+    assert str(video_stream_component.features_path) == os.path.join(os.getcwd(), *[".storage", "features.pt"])
